dcf9bcaaf4785d5caffbcd9a41e2995d525a3e68
can inan <cninnworks@gmail.com>
2023-09-23 19:18:16 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const is = require("simple-is")
+
+console.log(is.number(24),"evet bu değer bir sayıdır");
+console.log(is.string(2)?"evet bu bir string ifadedir":"hayır bu bir string ifade değildir.")
